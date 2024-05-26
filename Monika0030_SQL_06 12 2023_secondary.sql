56f0b04e34b26a0a6854e13591443c1eded8efcd
Monika0030 <153424794+Monika0030@users.noreply.github.com>
2024-05-26 14:45:42 +0200

    Delete Monika0030_SQL_06 12 2023_secondary.sql

--- Monika0030_SQL_06 12 2023_secondary.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TABLE monsimko_secondary AS
-SELECT 
-	ec.year AS country_year,
-	co.country AS country,
-	ec.GDP AS gdp
-FROM countries co 
-LEFT JOIN economies ec ON co.country = ec.country
-WHERE (ec.GDP IS NOT NULL OR ec.gini IS NOT NULL)
-ORDER BY country, ec.year;
